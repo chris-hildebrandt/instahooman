1a9412e79477b3a034f619348fcc1239e324da8a
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-12 15:33:39 -0600

    Co-authored-by: Tyler Truman <tylertruman@users.noreply.github.com> Co-authored-by: Garrett Adams <garrett-adamss@users.noreply.github.com> Co-authored-by: ChristineKlosterman <ChristineKlosterman@users.noreply.github.com> Co-authored-by: Zach Rasmussen <zachrasmussen@users.noreply.github.com>

--- client/app/Controllers/CommentsController.js
+++ client/app/Controllers/CommentsController.js
@@ -1,3 +1,55 @@
+import { ProxyState } from "../AppState";
+import { commentsService } from "../Services/CommentsService.js";
+import { Pop } from "../Utils/Pop.js";
+
+
+function _drawComment(){
+    let template= ''
+    ProxyState.comments.forEach(c => template += c.Template)
+    // @ts-ignore
+    document.getElementById('comments').innerHTML = template
+}
+
 export class CommentsController{
-  
+    
+    constructor(){
+        ProxyState.on('comments', _drawComment)
+        this.getComments()
+    }
+    async getComments() {
+        try {
+            await commentsService.getComments()
+        } catch (error) {
+            console.log('[Get Comments]', error)
+            Pop.error(error)
+        }
+    }
+
+    async createComment(){
+        try {
+            await commentsService.createComment()
+        } catch (error) {
+            console.log('[Create Comment]', error);
+            Pop.error(error)
+        }
+    }
+    async editComment(){
+        try {
+            
+        } catch (error) {
+            console.log('[Edit Comment]', error);
+            Pop.error(error)
+        }
+    }
+
+    async deleteComment(){
+        try {
+            
+        } catch (error) {
+            console.log('[Delete Comment]', error)
+            Pop.error(error)
+        }
+    }
+
+
 }
