de31ed8bfbd3a49e53c498523dc373e08f6a5ec1
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-12 17:13:44 -0600

    Co-authored-by: Tyler Truman <tylertruman@users.noreply.github.com> Co-authored-by: Garrett Adams <garrett-adamss@users.noreply.github.com> Co-authored-by: ChristineKlosterman <ChristineKlosterman@users.noreply.github.com> Co-authored-by: Zach Rasmussen <zachrasmussen@users.noreply.github.com>

--- client/app/Services/CommentsService.js
+++ client/app/Services/CommentsService.js
@@ -2,7 +2,27 @@ import { ProxyState } from "../AppState.js";
 import { Comment } from "../Models/Comment.js";
 import { api } from "./AxiosService.js";
 
-class CommentsService{
+class CommentsService {
+    async getComment() {
+        let res = await api.get('api/comments')
+        ProxyState.comments = res.data.map(c => new Comment(c))
+            //TODO CHECK res.data.map if correct info is being pulled in ^^^^
+    }
+    async createComment() {
+        let res = await api.post('api/comments')
+        let newComment = new Comment(res.data) //TODO CHECK res.data
+        ProxyState.comments = [...ProxyState.comments, newComment]
+    }
+    // async editComment(commentEdit) {
+    //     let res = await api.put(`api/comments/${commentEdit.Id}`, commentEdit)
+    //     let newComment = new Comment(res.data) //TODO CHECK res.data
+    //     let commentIndex = ProxyState.comments.findIndex(c => c.id == commentEdit.id)
+    //     ProxyState.comments.splice(commentIndex, 1, newComment)
+    //     ProxyState.comments = ProxyState.comments
+    // }
+    async deleteComment(commentId) {
+        letw 
+    }
 
 }
 
